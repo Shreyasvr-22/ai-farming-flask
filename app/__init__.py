7ddfaf102d9959857d22270ad2ffd79594730de6
unknown <shreyas.vr2004@gmail.com>
2025-07-31 22:14:18 +0530

    Enable cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, app
 from flask_cors import CORS  # ✅ Import this
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask("ai_farming_project")
     CORS(app)  # ✅ Enable CORS for all routes
 
     from .routes import main
-    app.register_blueprint(main)
+    app.register_blueprint(main)git add .
+git commit -m "Enable CORS"
+git push origin main
+
 
     return app
 CORS(app, resources={r"/*": {"origins": "http://10.189.250.200:3000"}})
